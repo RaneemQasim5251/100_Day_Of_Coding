2516bfae5c5180f58096f39cf7655230ab002fed
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2024-08-18 13:32:14 +0300

    Create Day_1_Project.py

---
+++ Day_1_Project.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What is the city you grow up in?\n")
+pit = input("What is the name of your pit?\n")
+print("Your band name could be " + city + " " + pit)
